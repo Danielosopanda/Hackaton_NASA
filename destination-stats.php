d789376882a27041885e8b66a78148a30098a194
Danielosopanda <danielmtzgro@hotmail.com>
2023-10-08 05:42:22 -0600

    Se modificó la interfaz de station.css

--- destination-stats.php
+++ destination-stats.php
@@ -15,9 +15,11 @@
     $numViajesQuery = "SELECT COUNT(*) numViajes FROM viaje v, estacion e, destino d WHERE e.idEstacion = v.idEstacion AND e.idDestino = d.idDestino AND e.idDestino = $idDestination;";
     $numViajes = $conexion->query($numViajesQuery)->fetch_object();
     
-    //echo $numViajes->numViajes;
+    echo "Número de viajes totales a este destino: ".$numViajes->numViajes."\n";
 
     $numViajesYearQuery = "SELECT COUNT(*) numViajes FROM viaje v, estacion e, destino d WHERE e.idEstacion = v.idEstacion AND e.idDestino = d.idDestino AND e.idDestino = $idDestination AND YEAR(v.fechaViaje) = YEAR(CURDATE());";
     $viajesYear = $conexion->query($numViajesYearQuery)->fetch_object();
+
+    echo "Número de viajes de este año a este destino : ".$viajesYear->numViajes;
 
 ?>
