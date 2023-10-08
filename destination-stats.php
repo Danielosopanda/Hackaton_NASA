5221a05d4ff0f2ebc34cd83941822fc4b3ce5654
gonmgro <gonzalosjr2003@gmail.com>
2023-10-08 05:11:48 -0600

    Se añadió la consulta para el numero de viajes por mes a un destino en destination-stats.php

--- destination-stats.php
+++ destination-stats.php
@@ -12,12 +12,19 @@
     $destinationName = $destination->nombreDestino;
     // echo $destinationName;
 
+    //Numero de viajes totales
     $numViajesQuery = "SELECT COUNT(*) numViajes FROM viaje v, estacion e, destino d WHERE e.idEstacion = v.idEstacion AND e.idDestino = d.idDestino AND e.idDestino = $idDestination;";
     $numViajes = $conexion->query($numViajesQuery)->fetch_object();
     
     //echo $numViajes->numViajes;
 
+    //Numero de viajes por año
     $numViajesYearQuery = "SELECT COUNT(*) numViajes FROM viaje v, estacion e, destino d WHERE e.idEstacion = v.idEstacion AND e.idDestino = d.idDestino AND e.idDestino = $idDestination AND YEAR(v.fechaViaje) = YEAR(CURDATE());";
     $viajesYear = $conexion->query($numViajesYearQuery)->fetch_object();
 
+    //Numero de viajes ese mes
+    $numViajesMonthQuery = "SELECT COUNT(*) numViajes FROM viaje v, estacion e, destino d WHERE e.idEstacion = v.idEstacion AND e.idDestino = d.idDestino AND e.idDestino = $idDestination AND MONTH(v.fechaViaje) = MONTH(CURDATE());";
+    $viajesMonth = $conexion->query($numViajesMonthQuery)->fetch_object();
+
+    //echo $viajesMonth->numViajes;
 ?>
